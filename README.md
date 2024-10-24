f38b8915af07fa48ee796144f7b10eba03d54e59
Jing Zhang <jzhang_fl@outlook.com>
2024-10-24 13:05:26 -0500

    Init check in

---
+++ README.md
@@ -0,0 +1,22 @@
+# TwoSum
+
+Given an array A and an integer target, find the indices of the two numbers in the array whose sum is equal to the given target.
+
+Note: The problem has exactly one solution. Do not use the same element twice.
+
+Example
+A: [1, 3, 3, 4]
+target: 5
+Answer: [0, 3]
+
+Testing
+Input Format
+The first line contains an integer ‘T’ denoting the number of test cases.
+
+For each test case, the input has three lines:
+
+An integer ’n’ denoting the length of the array.
+n space-separated integers denoting the elements of the array.
+An integer ‘target’ denoting the target value.
+Output Format
+For each test case, the output has two space-separated integers ‘i’ and ‘j’ denoting the indices of the array.
---
+++ TwoSum/TwoSum.cpp
@@ -0,0 +1,43 @@
+// TwoSum.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+
+std::pair<int, int> TwoSum(std::vector<int>& vec, int target)
+{
+    for (size_t i = 0; i < vec.size(); ++i)
+    {
+        for (size_t j = i + 1; j < vec.size(); ++j)
+        {
+            if (vec[i] + vec[j] == target)
+                return std::make_pair(static_cast<int>(i), static_cast<int>(j));
+        }
+    }
+
+    throw std::exception("No match found!");
+}
+
+int main()
+{
+    while (true)
+    {
+        int count = 0;
+        std::cout << "Number of elements in the array: ";
+        std::cin >> count;
+        if (count < 2)
+            break;
+
+        std::vector<int> vec(count);
+        std::cout << "The number array: ";
+        for (int i = 0; i < count; ++i)
+            std::cin >> vec[i];
+
+        std::cout << "The sum to find (target): ";
+        int target;
+        std::cin >> target;
+
+        std::pair<int, int> match = TwoSum(vec, target);
+        std::cout << "Result: " << match.first << ", " << match.second << std::endl;
+    }
+}
